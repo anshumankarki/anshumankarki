9e88a714d251b73a6b98f9d0d4721609bd8e1b6f
suman karki <sumankarki532@gmail.com>
2022-02-14 09:24:03 +0545

    remove_redundancy

--- remove_redundancy.py
+++ remove_redundancy.py
@@ -1,4 +1,7 @@
 #remove duplicate using for loop
 mylist = [1,4,2,5,2,1,4,9,7,5]
 newlist = []
-# for i in mylist:
+for i in mylist:
+    if i not in newlist:
+        newlist.append(i)
+print(str(newlist))        
